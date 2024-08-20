0aefcae000ba31a3b359ea80feaf030a20381e30
mikinovation <maneuver2472@gmail.com>
2024-08-20 22:36:33 +0800

    feat: apiサーバーの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  eslint: {
-    config: {
-      stylistic: true,
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
+    eslint: {
+        config: {
+            stylistic: true,
+        },
     },
-  },
 })
---
+++ server/api/users/login.post.ts
@@ -0,0 +1,5 @@
+// TODO: Implement the handler for this event
+
+export default defineEventHandler((event) => {
+    return `handler`
+})
